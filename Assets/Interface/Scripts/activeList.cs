c7f81e2b0292a336b37555bccad6f12f62b9dd7c
Maram M <bmaram@outlook.sa>
2019-07-07 19:09:17 +0300

    Adding Matthew project
    adding matthew's project after editing it to create and read multiple maps.

---
+++ Assets/Interface/Scripts/activeList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeList : MonoBehaviour
+{
+
+    public void ListT(GameObject p1){
+        p1.SetActive(true);
+    }
+
+    public void ListF(GameObject p1){
+        p1.SetActive(false);
+    }
+}
---
+++ Assets/Interface/Scripts/moves.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class moves : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Test("T1")); 
+    }
+
+    IEnumerator Test(string ss) {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(ss);
+    }
+}
